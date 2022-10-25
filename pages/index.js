60452b3e400c06a599afe3e692a2d0de4f6951c8
Alisha Evans <alisha@notch8.com>
2022-10-25 12:11:01 -0500

    returning the first 4 supplier services as the featured services

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,11 @@ import {
 } from 'webstore-component-library'
 import hero from '../assets/img/hero.jpg'
 import { fetcher } from '../services/fetcher'
-import { ITEMS, TEXT, TITLE } from '../constants/home'
+import { TEXT, TITLE } from '../constants/home'
 
 const Home = () => {
 	const [ query, setQuery ] = useState('')
+
 	// TODO(alishaevn): once the api is updated to accept a query with this path, we will want to use the second "url" declaration instead.
 	// with url being a function, the value won't be cached. it will run whenever we return to this page.
 	const url = () => `/providers/${process.env.NEXT_PUBLIC_PROVIDER_ID}/wares.json`
@@ -23,9 +24,22 @@ const Home = () => {
 
 	const handleOnSubmit = ({ value }) => setQuery(value)
 
+	// this is a manual search query until the api is updated to be able to return this for us
 	const wares = data?.ware_refs.filter((ware) => {
 		const queryExpression = new RegExp(query, 'i')
 		return queryExpression.test(ware.name)
+	})
+
+	const featured_services = data?.ware_refs.slice(0, 4).map(ware => {
+		return {
+			id: ware.reference_of_id,
+			description: ware.snippet,
+			img: {
+				src: ware.promo_image,
+				alt: `The promotional image for ${ware.name}`
+			},
+			name: ware.name,
+		}
 	})
 
 	return(
@@ -41,7 +55,7 @@ const Home = () => {
 			{/* TODO(alishaevn): remove the line below once we add the /browse route */}
 			{wares && wares.map(w => <p>{`${w.name}`}</p>)}
       <TitledTextBox title={TITLE} text={TEXT} />
-      <ItemGroup items={ITEMS} />
+      {featured_services && <ItemGroup items={featured_services} />}
 		</>
 	)
 }
