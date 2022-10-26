2e1396c2f3d913134365db1df16a3db464c6a5ce
Alisha Evans <alisha@notch8.com>
2022-10-26 12:04:39 -0500

    WIP: the search and services show on the browse page, but there are a few TODOs

---
+++ pages/browse.js
@@ -0,0 +1,65 @@
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import useSWR from 'swr'
+import {
+  Item,
+  SearchBar,
+} from 'webstore-component-library'
+import { fetcher } from '../services/fetcher'
+
+const Browse = () => {
+	// TODO(alishaevn): the code before the return is copied from the index page. we should use some global state instead
+	// TODO(alishaevn): add styling
+
+	const [ query, setQuery ] = useState('')
+	const url = () => `/providers/${process.env.NEXT_PUBLIC_PROVIDER_ID}/wares.json`
+	const { data, error } = useSWR(url, fetcher)
+
+	const handleOnSubmit = ({ value }) => setQuery(value)
+
+	const wares = data?.ware_refs.filter((ware) => {
+		const queryExpression = new RegExp(query, 'i')
+		return queryExpression.test(ware.name)
+	})
+
+	const services = wares?.map(ware => {
+		return {
+			description: ware.snippet,
+			id: ware.reference_of_id,
+			img: {
+				src: ware.promo_image,
+				alt: `The promotional image for ${ware.name}`
+			},
+			name: ware.name,
+			slug: ware.slug,
+		}
+	})
+
+	return(
+		<>
+			<SearchBar onSubmit={handleOnSubmit} />
+			{services && services.map(service => {
+				const { description, id, img, name } = service
+
+				return (
+					<Item
+						description={description}
+						key={id}
+						img={img}
+						name={name}
+						withButtonLink={true}
+						orientation='horizontal'
+						buttonProps={{
+							backgroundColor: '#A9A9A9',
+							label: 'Request this item',
+							primary: true,
+						}}
+						style={{ marginBottom: 15 }}
+					/>
+				)
+			})}
+		</>
+	)
+}
+
+export default Browse
