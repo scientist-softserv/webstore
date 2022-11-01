57cf67a0a6245851dae163f2d73fc67413e17936
Alisha Evans <alisha@notch8.com>
2022-11-01 10:30:19 -0500

    move "/browse" to "/browse/index" because you cannot have more than one dynamic route at a certain endpoint.

--- pages/browse/index.js
+++ pages/browse/index.js
@@ -1,11 +1,10 @@
-import { useRouter } from 'next/router'
 import { useState } from 'react'
 import useSWR from 'swr'
 import {
   Item,
   SearchBar,
 } from 'webstore-component-library'
-import { fetcher } from '../services/fetcher'
+import { fetcher } from '../../services/fetcher'
 
 const Browse = () => {
 	// TODO(alishaevn): the code before the return is copied from the index page. we should use some global state instead
@@ -38,9 +37,7 @@ const Browse = () => {
 	return(
 		<>
 			<SearchBar onSubmit={handleOnSubmit} />
-			{services && services.map(service => {
-
-				return (
+			{services && services.map(service => (
 					<Item
 						item={service}
 						withButtonLink={true}
@@ -52,10 +49,46 @@ const Browse = () => {
 						}}
 						style={{ marginBottom: 15 }}
 					/>
-				)
-			})}
+				))}
 		</>
 	)
 }
 
 export default Browse
+
+// export async function getStaticProps() {
+	// const [ query, setQuery ] = useState('')
+	// const url = () => `/providers/${process.env.NEXT_PUBLIC_PROVIDER_ID}/wares.json`
+	// const { data, error } = useSWR(url, fetcher)
+
+	// const query_results = data?.ware_refs.filter((ware) => {
+	// 	const queryExpression = new RegExp(query, 'i')
+	// 	return queryExpression.test(ware.name)
+	// })
+
+	// const wares = query_results?.map(ware => {
+	// 	return {
+	// 		description: ware.snippet,
+	// 		id: ware.reference_of_id,
+	// 		img: {
+	// 			src: ware.promo_image,
+	// 			alt: `The promotional image for ${ware.name}`
+	// 		},
+	// 		name: ware.name,
+	// 		slug: ware.slug,
+	// 	}
+	// })
+
+  // Call an external API endpoint to get posts
+  // const res = await fetch('https://.../posts')
+  // const posts = await res.json()
+
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+//   return {
+//     props: {
+//       data,
+// 			error,
+//     },
+//   }
+// }
