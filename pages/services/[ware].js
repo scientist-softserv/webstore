2e1402d205b764fc4f39d0775262d992818c19a7
Alisha Evans <alisha@notch8.com>
2022-11-01 10:29:34 -0500

    move "/[ware]" to "/services/[ware]" because you cannot have more than one dynamic route at a certain endpoint.

--- pages/services/[ware].js
+++ pages/services/[ware].js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router'
 const Ware = () => {
   const router = useRouter()
   const { ware } = router.query
+  console.log( 'ware:', router.query)
 
   return <p>Ware: {ware}</p>
 }
