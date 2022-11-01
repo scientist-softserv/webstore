e7c9f961f3226afea4d79e29b4cb129f312da620
Alisha Evans <alisha@notch8.com>
2022-11-01 17:03:18 -0500

    pressing the search button on the home page redirects to the browse page with the correct query. the query then does an auto search

--- pages/browse/index.js
+++ pages/browse/index.js
@@ -1,17 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import useSWR from 'swr'
-import {
-  Item,
-  SearchBar,
-} from 'webstore-component-library'
+import { Item, SearchBar } from 'webstore-component-library'
 import { fetcher } from '../../services/fetcher'
 
+// TODO(alishaevn): the code before the return is copied from the index page. we may need global state instead
+// TODO(alishaevn): add styling
+// TODO(alishaevn): redirect from this page to the correct request page
 const Browse = () => {
-	// TODO(alishaevn): the code before the return is copied from the index page. we should use some global state instead
-	// TODO(alishaevn): add styling
-
+	const router = useRouter()
 	const [ query, setQuery ] = useState('')
+
+	useEffect(() => {
+		if(router.query.q) setQuery(value)
+	}, [])
+
+	// TODO(alishaevn): once the api is updated to accept a query with this path, we will want to use the second "url" declaration instead.
+	// the null value prevents auto search on page load
 	const url = () => `/providers/${process.env.NEXT_PUBLIC_PROVIDER_ID}/wares.json`
+	// const url = () => query.length ? `/providers/${process.env.NEXT_PUBLIC_PROVIDER_ID}/wares.json&q=${query}` : null
 	const { data, error } = useSWR(url, fetcher)
 
 	const handleOnSubmit = ({ value }) => setQuery(value)
@@ -55,40 +62,3 @@ const Browse = () => {
 }
 
 export default Browse
-
-// export async function getStaticProps() {
-	// const [ query, setQuery ] = useState('')
-	// const url = () => `/providers/${process.env.NEXT_PUBLIC_PROVIDER_ID}/wares.json`
-	// const { data, error } = useSWR(url, fetcher)
-
-	// const query_results = data?.ware_refs.filter((ware) => {
-	// 	const queryExpression = new RegExp(query, 'i')
-	// 	return queryExpression.test(ware.name)
-	// })
-
-	// const wares = query_results?.map(ware => {
-	// 	return {
-	// 		description: ware.snippet,
-	// 		id: ware.reference_of_id,
-	// 		img: {
-	// 			src: ware.promo_image,
-	// 			alt: `The promotional image for ${ware.name}`
-	// 		},
-	// 		name: ware.name,
-	// 		slug: ware.slug,
-	// 	}
-	// })
-
-  // Call an external API endpoint to get posts
-  // const res = await fetch('https://.../posts')
-  // const posts = await res.json()
-
-  // By returning { props: { posts } }, the Blog component
-  // will receive `posts` as a prop at build time
-//   return {
-//     props: {
-//       data,
-// 			error,
-//     },
-//   }
-// }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRouter } from 'next/router'
 import useSWR from 'swr'
 import {
   Image,
@@ -11,24 +11,17 @@ import { fetcher } from '../services/fetcher'
 import { TEXT, TITLE } from '../constants/home'
 
 const Home = () => {
-	const [ query, setQuery ] = useState('')
+	const router = useRouter()
 
-	// TODO(alishaevn): once the api is updated to accept a query with this path, we will want to use the second "url" declaration instead.
 	// with url being a function, the value won't be cached. it will run whenever we return to this page.
+	// TODO(alishaevn): unless we find a way to pass in the featured_services instead, we will need to call the api on page load
+	// or we could decide to only poll for updates at certain intervals
 	const url = () => `/providers/${process.env.NEXT_PUBLIC_PROVIDER_ID}/wares.json`
-	// the null value prevents auto search on page load
-	// const url = () => query.length ? `/providers/${process.env.NEXT_PUBLIC_PROVIDER_ID}/wares.json&q=${query}` : null
 
 	// TODO(alishaevn): add error handling
 	const { data, error } = useSWR(url, fetcher)
 
-	const handleOnSubmit = ({ value }) => setQuery(value)
-
-	// this is a manual search query until the api is updated to be able to return this for us
-	const wares = data?.ware_refs.filter((ware) => {
-		const queryExpression = new RegExp(query, 'i')
-		return queryExpression.test(ware.name)
-	})
+	const handleOnSubmit = ({ value }) => router.push({ pathname: '/browse', query: { q: value } }, '/browse')
 
 	const featured_services = data?.ware_refs.slice(0, 4).map(ware => {
 		return {
@@ -53,8 +46,6 @@ const Home = () => {
         style={{ objectFit: 'cover' }}
       />
       <SearchBar onSubmit={handleOnSubmit} />
-			{/* TODO(alishaevn): remove the line below once we add the /browse route */}
-			{wares && wares.map(w => <p>{`${w.name}`}</p>)}
       <TitledTextBox title={TITLE} text={TEXT} />
 			{featured_services && (
 				<ItemGroup
