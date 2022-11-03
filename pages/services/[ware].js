f0623227ccafb566be95d707e8163c2b8a187713
Alisha Evans <alisha@scientist.com>
2022-11-03 09:49:28 -0500

    remove console log from pages/services/[ware].js

--- pages/services/[ware].js
+++ pages/services/[ware].js
@@ -3,7 +3,6 @@ import { useRouter } from 'next/router'
 const Ware = () => {
   const router = useRouter()
   const { ware } = router.query
-  console.log( 'ware:', router.query)
 
   return <p>Ware: {ware}</p>
 }
