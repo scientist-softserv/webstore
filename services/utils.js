cd0196981c35ae4664741f16914fdbecb1f82ff9
Alisha Evans <alisha@notch8.com>
2022-11-08 18:51:55 -0600

    use the DEFAULT_WARE_IMAGE as a fallback when configuring services

--- services/utils.js
+++ services/utils.js
@@ -4,13 +4,14 @@ import { DEFAULT_WARE_IMAGE } from '../constants'
 
 export const configure_services = ({ data, path }) => {
   return data?.map(ware => {
+    const img = ware.promo_image
+      ? { src: ware.promo_image, alt: `The promotional image for ${ware.name}` }
+      : DEFAULT_WARE_IMAGE
+
     return {
       description: ware.snippet,
       id: ware.reference_of_id,
-      img: {
-        src: ware.promo_image,
-        alt: `The promotional image for ${ware.name}`
-      },
+      img,
       name: ware.name,
       href: `${path}/${ware.slug}`,
     }
