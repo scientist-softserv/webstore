044a5e9de5cb9cd0a5cef826fbf8ae4d279c3a6a
Alisha Evans <alisha@notch8.com>
2022-11-10 14:22:24 -0600

    use ware.snippet if ware.description is empty

--- pages/services/[ware].js
+++ pages/services/[ware].js
@@ -18,7 +18,7 @@ const Service = () => {
         ) : (
           <ItemPage
             title={ware.name}
-            description={ware.description}
+            description={ware.description || ware.snippet}
             // TODO(alishaevn): update the below to an actual image once
             // https://github.com/assaydepot/scientist_api_v2/issues/184 is completed
             img={DEFAULT_WARE_IMAGE}
