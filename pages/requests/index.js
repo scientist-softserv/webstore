cdee86cf63b4f7744818ac7d9efa9a7e27ceac80
Alisha Evans <alisha@notch8.com>
2022-11-18 12:32:25 -0600

    rendering requests on the /requests route

--- pages/requests/index.js
+++ pages/requests/index.js
@@ -1,30 +1,18 @@
 import React from 'react'
 import { RequestList } from 'webstore-component-library'
-import { DEFAULT_WARE_IMAGE } from '../../utils/constants'
-import { getAllRequests } from '../../utils/api/requests'
-import { configure_requests } from '../../utils'
-import { statusColors } from '../../utils/theme/colors'
+import { getAllRequests, configure_requests } from '../../utils'
 
 const Requests = () => {
-  const { requests, isLoading, isError } = getAllRequests()
-  const featured_services = configure_requests({ data: requests, path: '/services' })?.slice(0, 3)
-  const status= {
-    text: 'Vendor Review',
-    backgroundColor: statusColors[status],
-  }
+  const { all_requests, isLoading, isError } = getAllRequests()
+  const requests = configure_requests({ data: all_requests, path: '/request' })
 
-  if (isError) return <h1>Error...</h1>
+  if (isError) return <h1>{`${isError.name}: ${isError.message}`}</h1>
 
   return (
     <>
       <RequestList
-        createdAt='September 9, 2022'
-        description='this is the description'
-        img={DEFAULT_WARE_IMAGE}
         isLoading={isLoading}
-        title='Title'
-        status={status}
-        updatedAt='September 9, 2022 at 9:21 am'
+        requests={requests}
       />
     </>
   )
