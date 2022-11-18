5f6e6b0388d58350cb918bf16f27e4a97dbc22d9
Alisha Evans <alisha@notch8.com>
2022-11-17 18:17:55 -0600

    set up the framework to allow for customizable brand colors and typography

---
+++ utils/theme/colors.js
@@ -0,0 +1,13 @@
+// TODO(alishaevn): expand on this thought
+export const primary = ''
+export const secondary = ''
+
+export const statusColors = {
+	'Vendor Review': 'green',
+	'Internal Review': '#DEAF17',
+}
+
+export const colors = {
+	primary,
+	secondary,
+}
