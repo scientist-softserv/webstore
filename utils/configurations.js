46b8e29108384e982dcc883bd3e151a19ec1c810
Alisha Evans <alisha@notch8.com>
2022-11-18 17:25:32 -0600

    write some regex to get a readable request description co-author: braydon justice <braydon@scientist.com.

--- utils/configurations.js
+++ utils/configurations.js
@@ -18,19 +18,35 @@ export const configure_services = ({ data, path }) => {
 }
 
 export const configure_requests = ({ data, path }) => {
-  return data?.map(request => ({
-    createdAt: request.created_at,
-    description: request.description,
-    href: `${path}/${request.id}`,
-    id: request.id,
-    img: DEFAULT_WARE_IMAGE,
-    title: `${request.identifier}: ${request.name}`,
-    status: {
-      // TODO(alishaevn): remove the fallback colors below once we're only receiving webstore applicable statuses
-      backgroundColor: statusColors[request.status]?.bg || light_grey,
-      text: request.status,
-      textColor: statusColors[request.status]?.text || black,
-    },
-    updatedAt: request.updated_at,
-  }))
+  return data?.map(request => {
+    const description = normalize_description(request.description)
+
+    return {
+      createdAt: request.created_at,
+      description,
+      href: `${path}/${request.id}`,
+      id: request.id,
+      img: DEFAULT_WARE_IMAGE,
+      title: `${request.identifier}: ${request.name}`,
+      status: {
+        // TODO(alishaevn): remove the fallback colors below once we're only receiving webstore applicable statuses
+        backgroundColor: statusColors[request.status]?.bg || light_grey,
+        text: request.status,
+        textColor: statusColors[request.status]?.text || black,
+      },
+      updatedAt: request.updated_at,
+    }
+  })
+}
+
+const normalize_description = (text) => {
+  // removes html elements, html comments and sections with 3 spaces from the string
+  const regex = /(<([^>]+)>)|\n|-+>|   /g
+  const description = text.replace(regex, '')
+  console.log({ description })
+  const length = 215
+
+  return description.length > length
+          ? `${description.substring(0, length - 3)}...`
+          : description
 }
