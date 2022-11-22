33e03d2f3491a1c8669b0ffdb20a2ca44ff664cc
Alisha Evans <alisha@notch8.com>
2022-11-21 18:12:43 -0600

    create the getCurrentUser function

---
+++ utils/api/user.js
@@ -0,0 +1,14 @@
+import useSWR from 'swr'
+import { fetcher } from './fetcher'
+
+// NOTE(alishaevn): this path is likely to change as a of result of next week's tech meeting, but this sets the foundation
+export const getCurrentUser = () => {
+  const { data, error } = useSWR(`/users/profile.json`, fetcher)
+	console.log({ data, error })
+
+  return {
+    user: data?.user,
+    userLoading: !error && !data,
+    userError: error,
+  }
+}
