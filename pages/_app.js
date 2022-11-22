2709526e0ec1e28e74bd55ef52c505a7f2497f99
Alisha Evans <alisha@notch8.com>
2022-11-21 18:14:40 -0600

    allow the current user to be accessible from every nested page

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,10 @@
 import { Footer, Header } from 'webstore-component-library'
-import { FOOTER_NAME, FOOTER_SECTIONS, FOOTER_SOCIALS, LOGO } from '../utils'
+import { FOOTER_NAME, FOOTER_SECTIONS, FOOTER_SOCIALS, getCurrentUser, LOGO } from '../utils'
 import '../styles/globals.css'
 
 // putting the header and footer here mean that they automatically surround every page
 const Webstore = ({ Component, pageProps }) => {
+  // TODO(alishaevn): also make the user accessible to the header
   return (
     <>
       <Header
@@ -13,7 +14,7 @@ const Webstore = ({ Component, pageProps }) => {
         logOutLink='/'
         requestsLink='/requests'
       />
-      <Component {...pageProps} />
+      <Component {...pageProps} {...getCurrentUser()} />
       <Footer
         companyName={FOOTER_NAME}
         sections={FOOTER_SECTIONS}
