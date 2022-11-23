e653fdd0ae461a9ae9a28ef1e489eb175600df95
Alisha Evans <alisha@notch8.com>
2022-11-23 10:27:41 -0600

    add comments to request routes to notate that we will want redirects for unauthenticated users

--- pages/requests/new/index.js
+++ pages/requests/new/index.js
@@ -1,4 +1,5 @@
 // TODO(alishaevn): redo this page when the time comes
+// TODO(alishaevn): trying to access this page without being signed in should redirect to the login page
 
 const NewBlankRequest = () => <h1>This is a new blank request.</h1>
 
