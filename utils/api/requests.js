2ec1cb1efb27957af0b69335d48600ae5a4e4110
Summer Cook <summer@scientist.com>
2022-11-23 15:02:36 -0600

    a bunch of comments

--- utils/api/requests.js
+++ utils/api/requests.js
@@ -3,9 +3,15 @@ import { fetcher } from './fetcher'
 
 export const getAllRequests = () => {
   const { data, error } = useSWR(`/quote_groups/mine.json`, fetcher)
+  // TODO: this hard codes a quote group ID for now, but it will need to dynamically receive the id from.. somewhere?
+  //const { sowData } = useSWR(`/quote_groups/512528/proposals.json`, fetcher)
+  // const { sowData } = useSWR(`/quote_groups/${data?.quote_group_refs?.id}/proposals.json`, fetcher)
+  console.log({data: data})
+  // console.log({sowData: sowData})
 
   return {
     all_requests: data?.quote_group_refs,
+    //sows: sowData,
     isLoading: !error && !data,
     isError: error,
   }
