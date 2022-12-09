e56544fe6261aee0cc2b4981d9f1a877dafe5f1c
Alisha Evans <alisha@notch8.com>
2022-12-09 16:37:36 -0600

    update the blank request page to use the blank request form component

--- pages/requests/new/index.js
+++ pages/requests/new/index.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
-import {
-  Title,
-} from 'webstore-component-library'
+import { BlankRequestForm } from 'webstore-component-library'
 
 // TODO(alishaevn): trying to access this page without being signed in should redirect to the login page
 
@@ -17,52 +15,20 @@ const NewBlankRequest = () => {
   }
   */
 
-  // have a function here that accepts our data and takes it to the post api call
-  // e.g. postApiCall(shippingAddress, billingAddress, billingSameAsShipping, description, proposedDeadline)
+  // have a real function here that accepts our data and takes it to the post api call
+  const postRequestForm = (requestForm) => {
+		console.log({ requestForm })
+	}
 
   // set up some local state
-  const [shippingAddress, setShippingAddress] = useState(null)
-  const [billingAddress, setBillingAddress] = useState(null)
-  const [billingSameAsShipping, setBillingSameAsShipping] = useState(null)
-  const [description, setDescription] = useState(null)
-  const [proposedDeadline, setProposedDeadline] = useState(null)
-
-  // set up a function that handles the data. the below won't work because we need an onChange for the fields
-  // or do we just use a react bootstrap form for this page??
-
-  // const handleData = ({ property, value }) => {
-  //   switch(property) {
-  //     case property === 'shippingAddress':
-  //       setShippingAddress(value)
-  //       break
-  //     case property === 'billingAddress':
-  //       setBillingAddress(value)
-  //       break
-  //     case property === 'billingSameAsShipping':
-  //       setBillingSameAsShipping(value)
-  //       break
-  //     case property === 'description':
-  //       setDescription(value)
-  //       break
-  //     case property === 'proposedDeadline':
-  //       setProposedDeadline(value)
-  //       break
-  //   }
-  // }
+  // const [shippingAddress, setShippingAddress] = useState(null)
+  // const [billingAddress, setBillingAddress] = useState(null)
+  // const [billingSameAsShipping, setBillingSameAsShipping] = useState(null)
+  // const [description, setDescription] = useState(null)
+  // const [proposedDeadline, setProposedDeadline] = useState(null)
 
   return(
-    <>
-      <Title title='New Request' />
-      {/* description component */}
-      {/* proposals due by component */}
-      <ShippingDetails
-        // onChange={({ property, value }) => handleData({ property, value })}
-      />
-
-      <Button
-        onClick={() => postApiCall(shippingAddress, billingAddress, billingSameAsShipping, description, proposedDeadline)}
-      />
-    </>
+    <BlankRequestForm onSubmit={requestForm => postRequestForm(requestForm)} />
   )
 }
 
