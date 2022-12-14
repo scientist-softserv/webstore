cc644f127336ed9723a2a371090bd054dd97a0bf
Alisha Evans <alisha@notch8.com>
2022-12-14 17:18:28 -0600

    add todos for us to come back to

--- pages/requests/new/index.js
+++ pages/requests/new/index.js
@@ -1,34 +1,19 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { BlankRequestForm } from 'webstore-component-library'
-
 // TODO(alishaevn): trying to access this page without being signed in should redirect to the login page
+// TODO(alishaevn): come back to this page once the initialize api function has been created. re: the thread below
+// https://assaydepot.slack.com/archives/C03FZDALABG/p1670605791891109
 
 const NewBlankRequest = () => {
-  /*
-  based on the json response when getting a request, lets assume that our post request to create a new request may accept a body with the following properties:
-  {
-    description,
-    billing_same_as_shipping,
-    proposed_deadline,
-    shipping_address,
-    billing_address
-  }
-  */
-
   // have a real function here that accepts our data and takes it to the post api call
   const postRequestForm = (requestForm) => {
-		console.log({ requestForm })
-	}
+    console.log({ requestForm })
+  }
 
-  // set up some local state
-  // const [shippingAddress, setShippingAddress] = useState(null)
-  // const [billingAddress, setBillingAddress] = useState(null)
-  // const [billingSameAsShipping, setBillingSameAsShipping] = useState(null)
-  // const [description, setDescription] = useState(null)
-  // const [proposedDeadline, setProposedDeadline] = useState(null)
-
-  return(
-    <BlankRequestForm onSubmit={requestForm => postRequestForm(requestForm)} />
+  return (
+    <div className='container'>
+      <BlankRequestForm onSubmit={requestForm => postRequestForm(requestForm)} />
+    </div>
   )
 }
 
