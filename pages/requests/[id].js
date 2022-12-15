00dd262ac4d845e26f8a8f120424250cad0d66a7
Summer Cook <summer@scientist.com>
2022-12-15 13:59:24 -0600

    add title and description

--- pages/requests/[id].js
+++ pages/requests/[id].js
@@ -2,6 +2,8 @@ import { useRouter } from 'next/router'
 import {
   Loading,
   StatusBar,
+  Title,
+  TextBox,
 } from 'webstore-component-library'
 import { getOneRequest, STATUS_ARRAY } from '../../utils'
 // TODO(alishaevn): trying to access this page without being signed in should redirect to the login page
@@ -13,10 +15,15 @@ const Request = () => {
 
   if (isLoadingRequest) return <Loading wrapperClass='item-page' />
   if (isRequestError) return <h1>{`${isError.name}: ${isError.message}`}</h1>
-  console.log('status::', request.status)
+  console.log('request::', request)
 
   return(
-    <StatusBar statusArray={STATUS_ARRAY} apiRequestStatus={request.status} />
+    <div className='container'>
+      <StatusBar statusArray={STATUS_ARRAY} apiRequestStatus={request.status} />
+      <Title title={request.name} addClass='mt-5'/>
+      {/* <TextBox text={request.description} addClass='mt-5'/> */}
+      <div dangerouslySetInnerHTML={{__html: request.description}} className='mt-2'/>
+    </div>
   )
 }
 
