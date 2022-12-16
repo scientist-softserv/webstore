ce57d194a1f1a31358d33cdd7606a4261eed25a0
Summer Cook <summer@scientist.com>
2022-12-16 14:53:09 -0600

    add responsive classes

--- pages/requests/[id].js
+++ pages/requests/[id].js
@@ -15,7 +15,7 @@ const Request = () => {
   const router = useRouter()
   const { id } = router.query
   const { request, isLoadingRequest, isRequestError } = GetOneRequest(id)
-  const {  allSOWs, isLoadingSOWs, isSOWError } = GetAllSOWs(id, request.identifier)
+  const {  allSOWs, isLoadingSOWs, isSOWError } = GetAllSOWs(id, request?.identifier)
   console.log(allSOWs)
 
   if (isLoadingRequest) return <Loading wrapperClass='item-page' />
@@ -25,10 +25,10 @@ const Request = () => {
   return(
     <div className='container'>
       <StatusBar statusArray={STATUS_ARRAY} apiRequestStatus={request.status} addClass='mt-4'/>
-      <div className='row'>
-        <div className='col-sm-3 mt-4'>
+      <div className='row mb-4'>
+        <div className='col-sm-4 col-md-3 mt-2 mt-sm-4 order-1 order-sm-0'>
           <ActionsGroup handleSendingMessages={handleSendingMessages}/>
-          <div className='mt-4'>
+          <div className='mt-3'>
             <RequestStats
               billingInfo={{ ...request.billingAddress }}
               createdAt={request.createdAt}
@@ -37,11 +37,11 @@ const Request = () => {
             />
           </div>
         </div>
-        <div className='col-sm-9 mt-4'>
+        <div className='col-sm-8 col-md-9 mt-4 order-0 order-sm-1'>
           <Title title={request.title}/>
           <CollapsibleSection header='Additional Information' description={request.htmlDescription}/>
           {allSOWs && allSOWs.map(document => (
-            <Document document={document}/>
+            <Document document={document} addClass='mt-3'/>
           ))}
         </div>
       </div>
