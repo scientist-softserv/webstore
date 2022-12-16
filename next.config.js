8e31cbe5c7220f2011d9258042e25aef8b8962bb
Alisha Evans <alisha@notch8.com>
2022-12-16 16:30:20 -0600

    run lint on the pages and utils directories in dev and prod

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds
+  },
 }
 
 module.exports = nextConfig
