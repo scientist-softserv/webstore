284517128b5e6b7df117e2484e6959adf56fbc6f
Alisha Evans <alisha@notch8.com>
2022-12-19 14:18:38 -0600

    lint fixes and env variable name updates

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,15 @@
-import NextAuth from "next-auth"
+import NextAuth from 'next-auth'
 
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     {
-      id: "scientist",
-      name: "Scientist",
-      type: "oauth",
-      authorization: "https://acme.scientist.com/oauth/authorize",
-      token: "https://acme.scientist.com/oauth/token",
-      profileUrl: "https://acme.scientist.com/users/profile.json",
+      id: 'scientist',
+      name: 'Scientist',
+      type: 'oauth',
+      authorization: 'https://acme.scientist.com/oauth/authorize',
+      token: 'https://acme.scientist.com/oauth/token',
+      profileUrl: 'https://acme.scientist.com/users/profile.json',
       profile(profile) {
         console.log(profile)
         // return profile
@@ -20,16 +20,16 @@ export const authOptions = {
           image: profile.scientist_account?.profile.profile_image_url,
         }
       },
-      clientId: process.env.NEXT_PUBLIC_SCIENTIST_ID,
-      clientSecret: process.env.NEXT_PUBLIC_SCIENTIST_SECRET,
-    } 
+      clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
+    }
     // ...add more providers here
   ],
   // callbacks: {
   //   async redirect({ url, baseUrl }) {
   //     console.log({url, baseUrl})
   //     // Allows relative callback URLs
-  //     if (url.startsWith("/")) return `${baseUrl}${url}`
+  //     if (url.startsWith('/')) return `${baseUrl}${url}`
   //     // Allows callback URLs on the same origin
   //     else if (new URL(url).origin === baseUrl) return url
   //     return baseUrl
