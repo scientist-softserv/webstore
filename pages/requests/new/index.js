d0bd2fe707ae7b0350601b31fa8fd1149889d522
Alisha Evans <alisha@notch8.com>
2022-12-22 17:05:05 -0600

    show a form on the new request pages

--- pages/requests/new/index.js
+++ pages/requests/new/index.js
@@ -5,14 +5,93 @@ import { BlankRequestForm } from 'webstore-component-library'
 // https://assaydepot.slack.com/archives/C03FZDALABG/p1670605791891109
 
 const NewBlankRequest = () => {
-  // TODO(alishaevn): put a real function here that accepts our data and takes it to the post api call
-  const postRequestForm = (requestForm) => {
-    console.log({ requestForm })
+  const initialState = {
+    name: 'New Request',
+    billingSameAsShipping: false,
+    proposedDeadline: null,
+    billing: {
+      street: '',
+      street2: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      country: '',
+      text: '',
+    },
+    shipping: {
+      street: '',
+      street2: '',
+      city: '',
+      state: '',
+      zipCode: '',
+      country: '',
+      text: '',
+    },
+    data: {
+      timeline: '',
+      description: '',
+      suppliersIdentified: 'Yes',
+    },
+    // TODO(alishaevn): how do we handle attachments?
   }
 
-  return (
+  const [requestForm, setRequestForm] = useState(initialState)
+
+  /**
+   * @param {object} event onChange event
+   * @param {string} property dot notated string representing the property in initialValue
+   * @returns {object} the updated component state
+   */
+  const updateRequestForm = (value, property) => {
+    const [initialProperty, nestedProperty] = property.split('.')
+
+    setRequestForm((currentState) => {
+      const updatedState = nestedProperty
+        ? { [initialProperty]: { ...requestForm[initialProperty], [nestedProperty]: value } }
+        : { [initialProperty]: value }
+
+      return {
+        ...currentState,
+        ...updatedState,
+      }
+    })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    if (requestForm.billingSameAsShipping === true) {
+      Object.assign(requestForm.billing, requestForm.shipping)
+    }
+
+    // TODO(alishaevn): comment this back in when it's working
+    // createRequest(requestForm)
+  }
+
+  return(
     <div className='container'>
-      <BlankRequestForm onSubmit={requestForm => postRequestForm(requestForm)} />
+      <Title title='New Request' addClass='mt-4' />
+      <form onSubmit={handleSubmit} id='new-request-form'>
+        <BlankRequestForm updateRequestForm={updateRequestForm} />
+        <div className='row'>
+          <div className='col'>
+            <ShippingDetails
+              billingCountry={requestForm.billing.country}
+              shippingCountry={requestForm.shipping.country}
+              updateRequestForm={updateRequestForm}
+            />
+          </div>
+          <div className='col'>
+            <AdditionalInfo updateRequestForm={updateRequestForm} />
+          </div>
+        </div>
+        <Button
+          backgroundColor='primary'
+          addClass='my-4 ms-auto d-block'
+          label='Initiate Request'
+          type='submit'
+          size='large'
+        />
+      </form>
     </div>
   )
 }
