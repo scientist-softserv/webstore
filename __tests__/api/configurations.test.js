071fb0e1539ee5427b8288677c2f275332c1e524
Xavier Lange <xrlange@gmail.com>
2023-01-13 15:45:05 -0500

    tests pass

--- __tests__/api/configurations.test.js
+++ __tests__/api/configurations.test.js
@@ -3,5 +3,5 @@ import '@testing-library/jest-dom'
 
 test('normalizes the date', () => {
   let abnormal_date_string = '2022-11-21T17:16:20.491Z'
-  expect(normalizeDate(abnormal_date_string)).toContain('Nov 21 2022 at 11:16:20 AM');
+  expect(normalizeDate(abnormal_date_string)).toContain(' Nov 21 2022 at 12:16:20 PM');
 });
