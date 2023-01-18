a1281eff573e2c74b528f5ffb5db2e1f76e024fa
Alisha Evans <alisha@notch8.com>
2023-01-18 12:55:08 -0600

    able to sign in and get a session object!

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,10 +1,8 @@
 import NextAuth from 'next-auth'
-import { baseURL } from '../../../utils'
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
 const authOptions = {
-  // const authOptions = (req, res) => ({
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     {
@@ -17,92 +15,22 @@ const authOptions = {
       authorization: 'https://acme.scientist.com/oauth/authorize',
       token: 'https://acme.scientist.com/oauth/token',
       userinfo: {
-        request: () => {}
-      },
-      // userinfo: `${baseURL}/users/profile.json`,
-      // the profile url is the api endpoint. is that correct? do we even need it?
-      // profileUrl: `${baseURL}/users/profile.json`,
-      profile(profile) {
-        console.log(profile)
-        return profile
-        return {
-          // id: profile.id,
-          // name: profile.scientist_account?.profile.nickname,
-          // email: profile.scientist_account?.email,
-          // image: profile.scientist_account?.profile.profile_image_url,
+        // url: "https://example.com/oauth/userinfo",
+        // The result of this method will be the input to the `profile` callback.
+        async request(context) {
+          // context contains useful properties to help you make the request.
+          // return await makeUserinfoRequest(context)
+          console.log('userinfo request', context)
+          return context
         }
       },
-    }
-  ],
-  // pages: {
-    // signIn: `${rootPath}/login`,
-    // signOut: '/auth/signout',
-    // error: '/auth/error', // Error code passed in query string as ?error=
-    // verifyRequest: '/auth/verify-request', // (used for check email message)
-    // newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-  // },
-  // jwt: {
-  //   // The maximum age of the NextAuth.js issued JWT in seconds.
-  //   // Defaults to `session.maxAge`.
-  //   maxAge: 60 * 60 * 24 * 30,
-  //   // You can define your own encode/decode functions for signing and encryption
-  //   async encode() {},
-  //   async decode() {},
-  // },
-  session: {
-    strategy: 'jwt',
-  },
-  // useSecureCookies: true,
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      console.log({url, baseUrl})
-      // Allows relative callback URLs
-      if (url.startsWith('/')) return `${baseUrl}${url}`
-      // Allows callback URLs on the same origin
-      else if (new URL(url).origin === baseUrl) return url
-      return baseUrl
-    },
-    async signIn({ user, account, profile, email, credentials }) {
-      console.log({ user, account, profile, email, credentials })
-      const isAllowedToSignIn = true
-      if (isAllowedToSignIn) {
-        return true
-      } else {
-        // Return false to display a default error message
-        return false
-        // Or you can return a URL to redirect to:
-        // return '/unauthorized'
+      profile({ provider, tokens }) {
+        console.log(tokens);
+        return tokens.user
       }
     }
-  },
-  // cookies: {
-  //   pkceCodeVerifier: {
-  //     name: `${cookiePrefix}next-auth.pkce.code_verifier`,
-  //     options: {
-  //       httpOnly: true,
-  //       sameSite: 'lax',
-  //       path: '/',
-  //       secure: useSecureCookies,
-  //       maxAge: 900
-  //     }
-  //   },
-  //   state: {
-  //     name: `${cookiePrefix}next-auth.state`,
-  //     options: {
-  //       httpOnly: true,
-  //       sameSite: "lax",
-  //       path: "/",
-  //       secure: useSecureCookies,
-  //       maxAge: 900
-  //     },
-  //   },
-  // },
-  debug: true,
-// })
-}
+  ],
 
-// export default (req, res) => {
-//   return NextAuth(req, res, authOptions(req, res))
-// }
+}
 
 export default NextAuth(authOptions)
