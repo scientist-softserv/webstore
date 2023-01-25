c894fcad6fd0090525e2ea10e738b7776ef423af
Alisha Evans <alisha@notch8.com>
2023-01-25 15:11:39 -0600

    refactor the _app page so we can pass a user session to the header in the component library

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,47 @@
 import { Footer, Header } from '@scientist-softserv/webstore-component-library'
-import { SessionProvider, signIn, signOut } from 'next-auth/react'
+import {
+  SessionProvider, signIn, signOut, useSession 
+} from 'next-auth/react'
+
 import {
   FOOTER_NAME,
   FOOTER_SECTIONS,
   FOOTER_SOCIALS,
   LOGO,
   NAVIGATION_LINKS,
-  useCurrentUser,
 } from '../utils'
 import '../utils/theme/globals.scss'
 
-// putting the header and footer here mean that they automatically surround every page
-const Webstore = ({ Component, pageProps: { session } }) => {
+const WebStore = ({ Component }) => {
+  const { data: session } = useSession()
+
   return (
-    <SessionProvider session={session}>
+    <>
       <Header
         auth={{
           signIn: () => signIn(process.env.NEXT_PUBLIC_PROVIDER_NAME),
-          signOut: () => signOut,
+          signOut: signOut,
         }}
         logo={LOGO}
         navLinks={NAVIGATION_LINKS}
-        // TODO(alishaevn): find the appropriate way to determine the session, user token, etc.
-        // currently, session is always returning "undefined" in this component
         userSession={session}
       />
-      <Component {...useCurrentUser()} />
+      <Component />
       <Footer
         companyName={FOOTER_NAME}
         sections={FOOTER_SECTIONS}
         socials={FOOTER_SOCIALS}
       />
+    </>
+  )
+}
+
+const App = ({ Component, pageProps: { session } }) => {
+  return (
+    <SessionProvider session={session}>
+      <WebStore Component={Component} />
     </SessionProvider>
   )
 }
 
-export default Webstore
+export default App
