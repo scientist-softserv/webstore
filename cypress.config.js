0f63749575f1136a0bafb566035b904607d218dc
Summer Cook <summer@scientist.com>
2023-02-09 18:04:53 -0600

    fix request test and test session env

--- cypress.config.js
+++ cypress.config.js
@@ -19,6 +19,6 @@ module.exports = defineConfig({
   env: {
     SCIENTIST_USER: 'test@test.com',
     SCIENTIST_PW: '!test1234',
-    TEST_SESSION_COOKIE: 'eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..uVYWk4VOIhaxlTQp.sHc7QiUM0Fdz9pogTe3-Q3Z8XqrJBjSu_-pP_N_qkGfrVHQ7_L-sI7WLPqqIYEd_zXZd6KQH69g--skZx0xG-f_p5PeTjPXt1UgNb3QuyMpMjv07v9zC3YUoovRX8zW1bDEsTr7Zqjcf5YwbDiyAatCsLg-q4jMwaAb_h24z5U6uwkM5vw-2jZ1VV5Nxg7eBTyabTeL_A0M3GFwtYDkEmJlN.Uv3WANjmyTJiPzUvV9OfAA'
+    TEST_SESSION_COOKIE: process.env.TEST_SESSION_COOKIE
   },
 });
--- cypress/e2e/requests.cy.js
+++ cypress/e2e/requests.cy.js
@@ -1,4 +1,13 @@
 describe("Requests Page", () => {
+  it("A logged out user should not able to see the requests list.", () => {
+		// Visit a protected route in order to allow cypress to set the cookie and mock the login
+		cy.visit("/requests")
+
+		cy.get('div.alert-heading').contains('Unauthorized').then(() => {
+			cy.log("A logged out user is not able to view requests.");
+		});
+	})
+  
 	it("A logged in user should be able to see the requests list.", () => {
 		// Call the custom cypress command to log in
 		cy.login(Cypress.env('SCIENTIST_USER'), Cypress.env('SCIENTIST_PW'))
@@ -7,7 +16,7 @@ describe("Requests Page", () => {
 		cy.visit("/requests")
 
 		cy.get('h1').contains('My Requests').then(() => {
-			cy.log("Test login successful");
+			cy.log("Successfully logged in and viewing requests page");
 		});
-	});
+	})
 });
