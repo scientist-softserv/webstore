e5e2bb24a9694df4a648b8d2c963bac0cfa80a0c
Summer Cook <summer@scientist.com>
2023-02-15 11:00:32 -0600

    update requests and browsing tests and move non secret env variables

--- cypress/e2e/browsing.cy.js
+++ cypress/e2e/browsing.cy.js
@@ -1,5 +1,5 @@
 describe('Browsing', () => {
-  it('a user can search from the home page and be directed to "/browse" with a blank query', () => {
+  it('completes a search from the home page and navigates to "/browse" with a blank query', () => {
     // Start from the home/index page
     cy.visit('/')
 
@@ -9,7 +9,27 @@ describe('Browsing', () => {
     // The new url should include "/browse"
     cy.url().should('include', '/browse')
 
+    // The new url should not contain a query
+    cy.url().should('not.include', '?')
+
     // The search bar on the browse page should remain blank
     cy.get('input.search-bar').should('have.value', '')
+  })
+
+  it('completes a search from the home page and navigates to "/browse" with a query term', () => {
+    // Start from the home/index page
+    cy.visit('/')
+
+    // type an example search into the searchbar
+    cy.get('input.search-bar').type('next')
+
+    // Press the search button
+    cy.get('button.search-button').click()
+
+    // The new url should include "/browse"
+    cy.url().should('include', '/browse?=next')
+
+    // The search bar on the browse page should have the text that was searched for
+    cy.get('input.search-bar').should('have.value', 'next')
   })
 })
--- cypress/e2e/requests.cy.js
+++ cypress/e2e/requests.cy.js
@@ -1,22 +1,25 @@
-describe("Requests Page", () => {
-  it("A logged out user should not able to see the requests list.", () => {
+describe('Viewing all requests', () => {
+  it('does not show a request list if the user is logged out.', () => {
 		// Visit a protected route in order to allow cypress to set the cookie and mock the login
 		cy.visit("/requests")
 
 		cy.get('div.alert-heading').contains('Unauthorized').then(() => {
-			cy.log("A logged out user is not able to view requests.");
-		});
+			cy.log("A logged out user is not able to view requests.")
+		})
 	})
   
-	it("A logged in user should be able to see the requests list.", () => {
+	it("shows the user's request list if they are logged in.", () => {
 		// Call the custom cypress command to log in
 		cy.login(Cypress.env('TEST_SCIENTIST_USER'), Cypress.env('TEST_SCIENTIST_PW'))
 
 		// Visit a protected route in order to allow cypress to set the cookie and mock the login
 		cy.visit("/requests")
 
-		cy.get('h1').contains('My Requests').then(() => {
-			cy.log("Successfully logged in and viewing requests page");
-		});
+		const requestListExists = cy.get('h1').contains('My Requests') || 
+    cy.get('p.no-requests').contains('You do not have any requests yet.')
+
+    requestListExists.then(() => {
+			cy.log('Successfully logged in and viewing request list')
+		})
 	})
-});
+})
