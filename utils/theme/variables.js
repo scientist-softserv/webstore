076df6fce379885af141d1f5f1203c3a9f490a7c
Alisha Evans <alisha@notch8.com>
2023-02-15 13:51:00 -0600

    add black and white to the bootstrap color map. also add 2 darkened versions of the secondary color. plus formatting in this file

--- utils/theme/variables.js
+++ utils/theme/variables.js
@@ -1,31 +1,45 @@
 import themeColors from './theme.module.scss'
-const { dark, primary, success, info, secondary, white } = themeColors
-export {
-  dark,
-  primary,
-  success,
+const {
+  black,
+  danger,
   info,
+  light,
+  primary,
+  dark,
   secondary,
+  success,
+  warning,
+  white,
+} = themeColors
+export {
+  black,
+  dark,
+  danger,
+  info,
+  light,
+  primary,
+  secondary,
+  success,
+  warning,
   white,
 }
 
-// These are the colors used for the requests
+// These are used for the request and document statuses
 export const statusColors = {
   'Supplier Review': {
-    bg: white,
+    bg: light,
     text: dark,
   },
   'SOW Selection': {
     bg: secondary,
-    text: white,
+    text: light,
   },
   'Work Started': {
     bg: primary,
-    text: white,
+    text: light,
   },
   'Work Completed': {
     bg: dark,
-    text: white,
+    text: light,
   },
 }
-
