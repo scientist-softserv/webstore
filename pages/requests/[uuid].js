c47e6beb293718499f69e7420c552500daf6c7ad
Alisha Evans <alisha@notch8.com>
2023-02-21 09:32:28 -0600

    rename the [id] file to [uuid] since that is the value we are now basing the page on

--- pages/requests/[uuid].js
+++ pages/requests/[uuid].js
@@ -29,13 +29,17 @@ import {
 const Request = () => {
   const router = useRouter()
   const { data: session } = useSession()
-  // parses the query string to get the quote group (request) id
-  const { id } = router.query
-  const { request, isLoadingRequest, isRequestError } = useOneRequest(id, session?.accessToken)
-  const { allSOWs, isLoadingSOWs, isSOWError } = useAllSOWs(id, request?.identifier, session?.accessToken)
-  const { messages, isLoadingMessages, isMessagesError, mutate, data } = useMessages(request?.uuid, session?.accessToken)
-  const { files, isLoadingFiles, isFilesError } = useFiles(id, session?.accessToken)
+  /**
+   * since this is a dynamically routed file, the router query will always consist of a "key: value" pair that's determined by the name of the file (key) and path string (value).
+   * additional query properties may also exist if they were explicitly passed.
+  */
+  const { uuid } = router.query
+  const { request, isLoadingRequest, isRequestError } = useOneRequest(uuid, session?.accessToken)
+  const { allSOWs, isLoadingSOWs, isSOWError } = useAllSOWs(uuid, request?.identifier, session?.accessToken)
+  const { messages, isLoadingMessages, isMessagesError, mutate, data } = useMessages(uuid, session?.accessToken)
+  const { files, isLoadingFiles, isFilesError } = useFiles(uuid, session?.accessToken)
   const documents = (allSOWs) ? [...allSOWs] : []
+  console.log({ request })
 
   const isLoading = isLoadingRequest || isLoadingSOWs || isLoadingFiles || isLoadingMessages
   const isError = isRequestError || isSOWError || isFilesError|| isMessagesError
