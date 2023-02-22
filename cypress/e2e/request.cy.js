4fdf2d0e090e9eebe64f6a5ad16e54f714aa6114
Diem Tran <diembtran@gmail.com>
2023-02-22 12:51:11 -0800

    finish test to view a single request

--- cypress/e2e/request.cy.js
+++ cypress/e2e/request.cy.js
@@ -7,27 +7,19 @@ describe('Viewing a single request', () => {
 		cy.visit("/requests")
     
     // check that either the requests are showing, or that the user has no requests
-		const requestListExists = cy.get('article.request-item').should('exist') || 
+		const requestListExists = cy.get('article.request-item').should('exist') ||
     cy.get('p.no-requests').contains('You do not have any requests yet.')
 
-    // if there are requests, click one (the first you see?)
+    // if there are requests, click the first one at the top of the list
     requestListExists.then(() => {
-			
-	})
-  
-	it("shows the user's request list if they are logged in.", () => {
-		// Call the custom cypress command to log in
-		cy.login(Cypress.env('TEST_SCIENTIST_USER'), Cypress.env('TEST_SCIENTIST_PW'))
+			cy.contains(': New Request').click()
+    })
 
-		// Visit a protected route in order to allow cypress to set the cookie and mock the login
-		cy.visit("/requests")
-
-    // check that either the requests are showing, or that the user has no requests
-		const requestListExists = cy.get('article.request-item').should('exist') || 
-    cy.get('p.no-requests').contains('You do not have any requests yet.')
-
-    requestListExists.then(() => {
-			cy.log('Successfully logged in and viewing request list')
-		})
+    // after clicking a request, check to make sure it navigated to the single request page successfully
+    if (cy.contains('Request Info')) {
+      cy.log('Successfully viewed a single request')
+    } else {
+      cy.log('Unsuccessfully viewed a single request')
+    }
 	})
 })
