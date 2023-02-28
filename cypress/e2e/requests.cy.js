24132b6cc5f174ecd419d5bc5d27fff86dfc9f4c
Summer Cook <summer@scientist.com>
2023-02-28 09:32:43 -0500

    update requests user flow tests

--- cypress/e2e/requests.cy.js
+++ cypress/e2e/requests.cy.js
@@ -1,26 +1,53 @@
 describe('Viewing all requests', () => {
-  it('does not show a request list if the user is logged out.', () => {
-		// Visit a protected route in order to allow cypress to set the cookie and mock the login
-		cy.visit("/requests")
-
-		cy.get('div.alert-heading').contains('Unauthorized').then(() => {
-			cy.log("A logged out user is not able to view requests.")
-		})
-	})
+  describe('as a logged out user', () => {
+    it('should show an error message.', () => {
+      // Visit a protected route in order to allow cypress to set the cookie and mock the login
+      cy.visit("/requests")
+      cy.get('div.alert-heading').contains('Unauthorized').then(() => {
+        cy.log("A logged out user is not able to view requests.")
+      })
+    })
+  })
   
-	it("shows the user's request list if they are logged in.", () => {
-		// Call the custom cypress command to log in
-		cy.login(Cypress.env('TEST_SCIENTIST_USER'), Cypress.env('TEST_SCIENTIST_PW'))
+  describe('as a logged in user', () => {
+    let scientistApiBaseURL = `https://${Cypress.env('NEXT_PUBLIC_PROVIDER_NAME')}.scientist.com/api/v2`
+    let requestList
 
-		// Visit a protected route in order to allow cypress to set the cookie and mock the login
-		cy.visit("/requests")
+    beforeEach(() => {
+      // Call the custom cypress command to log in
+      cy.login(Cypress.env('TEST_SCIENTIST_USER'), Cypress.env('TEST_SCIENTIST_PW'))
 
-    // check that either the requests are showing, or that the user has no requests
-		const requestListExists = cy.get('article.request-item').should('exist') || 
-    cy.get('p.no-requests').contains('You do not have any requests yet.')
+      // Intercept the response from the endpoint to view all requests
+      cy.intercept('GET', `${scientistApiBaseURL}/quote_groups/mine.json`, (req) => {
+        if (requestList === true) {
+          req.reply({fixture: 'all-requests/requests.json'})
+        } else {
+          req.reply({fixture: 'all-requests/no-requests.json'})
+        }
+      }).as('useAllRequests')
+      cy.visit("/requests")
+    })
 
-    requestListExists.then(() => {
-			cy.log('Successfully logged in and viewing request list')
-		})
-	})
+    context('has requests', () => {
+      before(() => {
+        requestList = true
+      })
+      it("shows the user's request list.", () => {
+        cy.get('article.request-item').should('exist').then(() => {
+          cy.log('Successfully viewing request list.')
+        })
+      })
+    })
+
+    context('has 0 requests', () => {
+      before(() => {
+        requestList = false
+      })
+      it("shows a message notifying the user they don't have any requests.", () => {
+        cy.get('p.no-requests').contains('You do not have any requests yet.').then(() => {
+          cy.log('Successfully viewing request page with no requests.')
+        })
+      })
+    })
+  })
 })
