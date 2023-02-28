ae1c3cbcb16687c80cdd0f7ec51dcd3db553f198
Summer Cook <summer@scientist.com>
2023-02-28 16:58:14 -0500

    test linked button component

--- cypress/e2e/requests.cy.js
+++ cypress/e2e/requests.cy.js
@@ -2,9 +2,9 @@ describe('Viewing all requests', () => {
   describe('as a logged out user', () => {
     it('should show an error message.', () => {
       // Visit a protected route in order to allow cypress to set the cookie and mock the login
-      cy.visit("/requests")
+      cy.visit('/requests')
       cy.get('div.alert-heading').contains('Unauthorized').then(() => {
-        cy.log("A logged out user is not able to view requests.")
+        cy.log('A logged out user is not able to view requests.')
       })
     })
   })
@@ -19,8 +19,8 @@ describe('Viewing all requests', () => {
     beforeEach(() => {
       // Call the custom cypress command to log in
       cy.login(Cypress.env('TEST_SCIENTIST_USER'), Cypress.env('TEST_SCIENTIST_PW'))
-
       // Intercept the response from the endpoint to view all requests
+      // TODO(summer-cook): extract out this base url into the config to use as an environment variable. it was not cooperating before
       cy.intercept('GET', `${scientistApiBaseURL}/quote_groups/mine.json`, (req) => {
         if ((requestList === undefined) && (loading === true)) {
           // reply with an empty response: both data and error will be undefined.
@@ -35,15 +35,18 @@ describe('Viewing all requests', () => {
           req.reply({ fixture: 'all-requests/no-requests.json' })
         }
       })
-      cy.visit("/requests")
+      // Intercept the response from the endpoint that gets the default ware ID
+      cy.intercept('GET', `${scientistApiBaseURL}/wares.json?q=make-a-request`, { fixture: 'all-requests/make-a-request.json' })
+      cy.visit('/requests')
     })
+
 
     context('request list is loading', () => {
       before(() => {
         loading = true
       })
-      it("should show a loading spinner.", () => {
-        cy.get('[aria-label="tail-spin-loading"]').should('be.visible').then(() => {
+      it('should show a loading spinner.', () => {
+        cy.get("[aria-label='tail-spin-loading']").should('be.visible').then(() => {
           cy.log('Loading spinner displays correctly.')
         })
       })
@@ -55,31 +58,46 @@ describe('Viewing all requests', () => {
         loading = undefined
         error = true
       })
-      it("should show an error message.", () => {
-        cy.get('div[role="alert"]').should('be.visible').then(() => {
+      it('should show an error message.', () => {
+        cy.get("div[role='alert']").should('be.visible').then(() => {
           cy.log('Successfully hits an error.')
         })
       })
     })
 
-    context('has requests', () => {
-      before(() => {
-        requestList = true
-      })
-      it("should show the user's request list.", () => {
-        cy.get('article.request-item').should('exist').then(() => {
-          cy.log('Successfully viewing request list.')
+    describe('request components are loading successfully, &', () => {
+      context('the user has requests', () => {
+        before(() => {
+          requestList = true
+        })
+        it("should show the user's request list.", () => {
+          cy.get('article.request-item').should('exist').then(() => {
+            cy.log('Successfully viewing request list.')
+          })
         })
       })
-    })
 
-    context('has 0 requests', () => {
-      before(() => {
-        requestList = false
+      context('the user has 0 requests', () => {
+        before(() => {
+          requestList = false
+        })
+        it("should show a message notifying the user they don't have any requests.", () => {
+          cy.get('p.no-requests').contains('You do not have any requests yet.').then(() => {
+            cy.log('Successfully viewing request page with no requests.')
+          })
+        })
       })
-      it("should show a message notifying the user they don't have any requests.", () => {
-        cy.get('p.no-requests').contains('You do not have any requests yet.').then(() => {
-          cy.log('Successfully viewing request page with no requests.')
+
+      context('the user can see the <LinkedButton /> component', () => {
+        [true, false].forEach( (value)=>{
+          before(() => {
+            requestList = value
+          })
+          it(`should show a button that links to the initialize request page for the default ware ${value ? 'with a request list' : 'with 0 requests'}.`, () => {
+            cy.get("a[data-cy='linked-button']").should('have.attr', 'href', `/requests/new/make-a-request?id=123`).then(() => {
+              cy.log('The <LinkedButton /> component displays correctly')
+            })
+          })
         })
       })
     })
