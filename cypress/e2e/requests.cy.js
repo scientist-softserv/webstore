2da872eb01db152e3098c370fd39cd19d276dd1e
Summer Cook <summer@scientist.com>
2023-02-28 15:35:34 -0500

    change example request status and update requests test to include loading and error cases

--- cypress/e2e/requests.cy.js
+++ cypress/e2e/requests.cy.js
@@ -11,7 +11,10 @@ describe('Viewing all requests', () => {
   
   describe('as a logged in user', () => {
     let scientistApiBaseURL = `https://${Cypress.env('NEXT_PUBLIC_PROVIDER_NAME')}.scientist.com/api/v2`
+    // declare variables that can be used to change how the response is intercepted.
     let requestList
+    let loading
+    let error
 
     beforeEach(() => {
       // Call the custom cypress command to log in
@@ -19,20 +22,51 @@ describe('Viewing all requests', () => {
 
       // Intercept the response from the endpoint to view all requests
       cy.intercept('GET', `${scientistApiBaseURL}/quote_groups/mine.json`, (req) => {
-        if (requestList === true) {
-          req.reply({fixture: 'all-requests/requests.json'})
-        } else {
-          req.reply({fixture: 'all-requests/no-requests.json'})
+        if ((requestList === undefined) && (loading === true)) {
+          // reply with an empty response: both data and error will be undefined.
+          req.reply()
+        } else if ((requestList === undefined) && (loading === undefined) && (error === true)) {
+          // error will be defined
+          req.reply({ statusCode: 404 })
+        } else if (requestList === true) {
+          // reply with a request body- default status code is 200
+          req.reply({ fixture: 'all-requests/requests.json' })
+        } else if (requestList === false) {
+          req.reply({ fixture: 'all-requests/no-requests.json' })
         }
-      }).as('useAllRequests')
+      })
       cy.visit("/requests")
+    })
+
+    context('request list is loading', () => {
+      before(() => {
+        loading = true
+      })
+      it("should show a loading spinner.", () => {
+        cy.get('[aria-label="tail-spin-loading"]').should('be.visible').then(() => {
+          cy.log('Loading spinner displays correctly.')
+        })
+      })
+    })
+
+    context('error while making a request to the api', () => {
+      before(() => {
+        requestList = undefined
+        loading = undefined
+        error = true
+      })
+      it("should show an error message.", () => {
+        cy.get('div[role="alert"]').should('be.visible').then(() => {
+          cy.log('Successfully hits an error.')
+        })
+      })
     })
 
     context('has requests', () => {
       before(() => {
         requestList = true
       })
-      it("shows the user's request list.", () => {
+      it("should show the user's request list.", () => {
         cy.get('article.request-item').should('exist').then(() => {
           cy.log('Successfully viewing request list.')
         })
@@ -43,7 +77,7 @@ describe('Viewing all requests', () => {
       before(() => {
         requestList = false
       })
-      it("shows a message notifying the user they don't have any requests.", () => {
+      it("should show a message notifying the user they don't have any requests.", () => {
         cy.get('p.no-requests').contains('You do not have any requests yet.').then(() => {
           cy.log('Successfully viewing request page with no requests.')
         })
