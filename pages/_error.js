82673727b93f88fba2540371960ed62924f56343
Alisha Evans <alisha@scientist.com>
2023-03-10 15:00:16 -0600

    remove unnecessary comment

--- pages/_error.js
+++ pages/_error.js
@@ -20,10 +20,6 @@ import * as Sentry from '@sentry/nextjs'
 import NextErrorComponent from 'next/error'
 
 const CustomErrorComponent = props => {
-  // If you're using a Nextjs version prior to 12.2.1, uncomment this to
-  // compensate for https://github.com/vercel/next.js/issues/8592
-  // Sentry.captureUnderscoreErrorException(props)
-
   return <NextErrorComponent statusCode={props.statusCode} />
 }
 
