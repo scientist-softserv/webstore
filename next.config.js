b79bc84da2121af7ea9a70b18ab6b35f28eef4c6
Alisha Evans <alisha@scientist.com>
2023-03-10 16:41:18 -0600

    remove commented out code

--- next.config.js
+++ next.config.js
@@ -35,14 +35,6 @@ const sentryWebpackPluginOptions = {
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 }
 
-
-// module.exports = withSentryConfig(
-//   module.exports,
-//   nextConfig,
-//   { silent: true },
-//   { hideSourceMaps: true },
-// )
-
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = withSentryConfig(
