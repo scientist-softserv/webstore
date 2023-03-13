c9c82ea3e4566b50bb41acf37e7f8d6ccfd0985c
Alisha Evans <alisha@scientist.com>
2023-03-13 09:38:43 -0500

    move all sentry related values to .env.local

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://78ce09ae2f694f0e8981cf5cec6fc2c7@o1008683.ingest.sentry.io/4504810271408128',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   // ref: https://develop.sentry.dev/sdk/performance/#sdk-configuration
   tracesSampleRate: 1.0,
