567731769b4fc20ca47ab7c990812f286f2a659d
Alisha Evans <alisha@scientist.com>
2023-03-13 11:23:59 -0500

    refactor sentry initialization

--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -4,10 +4,8 @@
 
 import * as Sentry from '@sentry/nextjs'
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
-
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   // ref: https://develop.sentry.dev/sdk/performance/#sdk-configuration
   tracesSampleRate: 1.0,
