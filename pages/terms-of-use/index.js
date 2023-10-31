9535eee1fa5bb3bbc28d03345d4bd42e08304cd2
Alisha Evans <alishaevn2@gmail.com>
2023-10-31 11:15:17 -0500

    add the terms of use page

---
+++ pages/terms-of-use/index.js
@@ -0,0 +1,13 @@
+import { TitledTextBox } from '@scientist-softserv/webstore-component-library'
+
+const TermsOfUse = () => (
+  <div className='container'>
+    <TitledTextBox title='Terms of Use' text={text} />
+  </div>
+)
+
+export default TermsOfUse
+
+const text = `
+Update this text with the actual terms of use from the client.
+`
