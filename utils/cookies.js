5cea42d91f249f7d6d45e1dad2017807edbba3d5
Alisha Evans <alishaevn2@gmail.com>
2023-11-29 16:13:35 -0600

    another cookies refactor
    pass an options object to setCookie instead of setting the value of the
    cookie as an object. This allows us to set the expiry date of the cookie
    and not have to manually delete it once expired.
    
    it also make some of the code a bit more readable.

--- utils/cookies.js
+++ utils/cookies.js
@@ -1,36 +1,46 @@
 import {
   deleteCookie,
   getCookie,
-  getCookies,
   hasCookie,
   setCookie,
 } from 'cookies-next'
 
-// once the feature is ready, uncomment the line below
 const cookieConsentGiven = hasCookie('_dl_cookie_consent')
-export const cookieConsentValue = cookieConsentGiven ? JSON.parse(getCookie('_dl_cookie_consent')).value : true
-const cookieConsent = {
-  expires: new Date().setFullYear(new Date().getFullYear() + 1), // 1 year from now in milliseconds
-  domain: process.env.NEXT_PUBLIC_PROVIDER_NAME,
+
+export const getCookieConsent = () => !cookieConsentGiven
+
+export const cookieConsentValue = cookieConsentGiven ? getCookie('_dl_cookie_consent') : 'true'
+
+/**
+ * setting the maxAge means that the cookie will be "deleted" after the maxAge has passed.
+ * it can still be found in dev tools, but hasCookie() will return false.
+ * therefore, we will be prompted in getCookieConsent() to ask for consent again.
+ */
+const cookieOptions = {
+  maxAge: 60 * 60 * 24 * 365, // 1 year in seconds
+  path: '/',
+  sameSite: 'lax',
 }
 
 export const enableCookies = () => {
-  const cookie = JSON.stringify({ value: true, ...cookieConsent })
+  setCookie('_dl_cookie_consent', 'true', cookieOptions)
   setCookie('_dl_cookie_consent', cookie, { path: '/' })
   // set other cookies
 }
 
+const nonEssentialCookies = [
+  '__ga',
+  '__gid',
+  '__utma',
+  '__utmt',
+  '__utmb',
+  '__utmc',
+  '__utmz',
+  '__utmv',
+  'pll_language'
+]
+
 export const disableCookies = () => {
-  const cookie = JSON.stringify({ value: false, ...cookieConsent })
-  // will account for this in a future pr
-  // Object.keys(getCookies()).forEach(cookie => deleteCookie(cookie))
-
-  setCookie('_dl_cookie_consent', cookie, { path: '/' })
-}
-
-export const getCookieConsent = () => {
-  const cookieExpiration = cookieConsentGiven ? JSON.parse(getCookie('_dl_cookie_consent')).expires : undefined
-
-  if ((!cookieConsentGiven) || (new Date() > cookieExpiration)) return true
-  return false
+  nonEssentialCookies.forEach(cookie => deleteCookie(cookie))
+  setCookie('_dl_cookie_consent', 'false', cookieOptions)
 }
