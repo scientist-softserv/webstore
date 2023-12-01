5bed6edbc2d0ecbe21b3e8c78509041c5c69dad4
Alisha Evans <alishaevn2@gmail.com>
2023-12-01 16:04:52 -0600

    update legal notice index page styling

--- pages/legal-notices/index.js
+++ pages/legal-notices/index.js
@@ -1,10 +1,21 @@
-import { LinkGroup, Title } from '@scientist-softserv/webstore-component-library'
+import { Link, Title } from '@scientist-softserv/webstore-component-library'
 import { LEGAL_NOTICES } from '../../utils'
 
 const LegalNotices = () => (
   <div className='container'>
     <Title title='Legal Notices' style={{ marginTop: '1rem' }} />
-    <LinkGroup links={LEGAL_NOTICES} />
+    {LEGAL_NOTICES.map(({ name, url }) => (
+      <Link
+        label={name}
+        href={url}
+        style={{
+          borderBottom: '1px solid',
+          marginBottom: '10px',
+          display: 'block',
+          paddingBottom: '5px'
+        }}
+      />
+    ))}
   </div>
 )
 
